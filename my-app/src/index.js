0ebca6faaaa07078fbec559dcb6e4a6959e043b1
sulmeee <poorooria@naver.com>
2022-06-25 19:36:57 +0900

    learn layout

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,7 @@
-// 20. Build a React info site
-// Styles and Images with CRA
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-// css import / 스타일로더로 쉽게 css 연결하기
+// css import / 스타일로더로 쉽게 css 연결하기ㅣㄷㅁㄱ
 import './index.css';
 import App from './App';
 import Header from './Header';
@@ -24,3 +21,52 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// 21. Build a React info site
+// Quick mental outline of project
+
+// import React from "react";
+// import ReactDOM from "react-dom"
+
+// function Page(){
+//     return (
+//         <>
+//         // 내 생각
+//         <Header>
+//             <img/>
+//             <span></span>
+//             <span></span>
+//         </Header>
+//         <Main>
+//             <h1></h1>
+//             <ul>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//                 <li></li>
+//             </ul>
+//         </Main>
+        
+//         // 풀이
+//         <div.container>
+//             <nav>
+//                 <img />
+//                 <h3></h3>
+//                 <h4></h4>
+//             </nav>
+//             <main>
+//                 <h1></h1>
+//                 <ul>
+//                     <li></li>
+//                     <li></li>
+//                     <li></li>
+//                     <li></li>
+//                     <li></li>
+//                 </ul>
+//             </main>
+//         </div.container>
+//         </>
+//     )
+// }
+// ReactDOM.render(<Page />, document.getElementById("root"))
