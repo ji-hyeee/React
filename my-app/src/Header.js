9b22ab0f6517efee6d475c94cb63d0f60791308e
sulmeee <poorooria@naver.com>
2022-06-29 22:34:22 +0900

    learn Main Section

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,13 +1,10 @@
 import './index.css'
-// image 도 import 로 연결해주기
 import logo from './logo.svg';
 
-// 이렇게 적어도 된다
 export default function Header() {
     return (
         <header>
             <nav className="nav">
-                {/* <img src=""./image/logo.svg" className="nav-logo" /> */}
                 <img src={logo} className="nav-logo" />
                 <h1 className="nav-text">ReactFacts</h1>
                 <ul className="nav-items">
@@ -19,6 +16,3 @@ export default function Header() {
         </header>
     )
 }
-
-// 이렇게 적어도 되지만
-// export default Header
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-// css import / 스타일로더로 쉽게 css 연결하기
 import './index.css';
 import App from './App';
 import Header from './Header';
@@ -17,20 +16,7 @@ root.render(
   </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// 22. Build a React info stie
-// Quick Figma Walkthrough
-// 피그마 튜토리얼
-
-// 23. Build a React info stie
-// Project Setup
-// 별도의 폴더를 만들었을 경우
-// ./폴더명./파일명
-
-// 24. Build a React info stie
-// Navbar and Styling
-// css 에는 기본요소보다는 className 을 사용하자
+// 25. Build a React info stie
+// Main Section
