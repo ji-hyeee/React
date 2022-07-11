790b4b5440326a875897f9f151ba335d8ca40d13
sulmeee <poorooria@naver.com>
2022-07-11 23:36:52 +0900

    learn Hero

--- section_2/src/App.js
+++ section_2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./compoenets/NavBar"
+import Hero from "./compoenets/Hero"
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
--- section_2/src/compoenets/NavBar.js
+++ section_2/src/compoenets/NavBar.js
@@ -10,8 +10,7 @@ export default function Navbar() {
 }
 
 // 2. Build an AirBnb Experiences Clone
-// Project Setup: NavBar
+// Project Setup: Hero
 
-// 프로젝트 상단 섹션 만들기 > 정적요소
-// 작은 사진 격자와 텍스트
-// 메뉴바와 히어로바로 나눌 것이다
+// Challenge: Build the Hero component.
+// Check the Figma file for the design specifics.
