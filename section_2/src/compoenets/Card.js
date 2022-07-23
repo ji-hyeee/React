f9ec946b817cb17c389396ca89278e04ef3fd52f
sulmeee <poorooria@naver.com>
2022-07-23 16:00:49 +0900

    learn Using JS in JSX

--- section_2/src/compoenets/Card.js
+++ section_2/src/compoenets/Card.js
@@ -1,6 +1,23 @@
 import React from "react"
 
-export default function Card(props) {
+export default function Card() {
+
+  const firstName = "박"
+  const lastName = "강현"
+
+  const date = new Date()
+  const hours = date.getHours()
+
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
   return (
     <div className="card">
       <img src="https://pbs.twimg.com/media/FYMzp5LVEAAqFVu?format=jpg&name=large" alt="card1" className="card--image" />
@@ -12,41 +29,15 @@ export default function Card(props) {
       </div>
       <p>Life Lessons with Park Kang Hyun</p>
       <p><span className="bold">From $136</span> / person</p>
+      <div>
+        {/* 2. Build an AirBnb Experiences Clone */}
+        {/* Aside: JS Inside JSX */}
+        <h1>Hello {firstName}{lastName} !</h1>
+        <h1>It is currently about {date.getHours() % 12} o'clock !</h1>
+        <h1>It is currently about {new Date().getHours() % 12} o'clock !</h1>
+        <h1>It is currently about {hours} o'clock !</h1>
+        <h1>Good {timeOfDay}!</h1>
+      </div>
     </div>
   )
 }
-
-// 2. Build an AirBnb Experiences Clone
-// Problem - Not Reusable
-
-// 하드코딩으로 재사용할 수 없다
-// 반복적인 구성 요소는 어디서나 볼 수 있다
-// 자동적으로 업데이트 되도록 만들어보자
-// props
-
-// 2. Build an AirBnb Experiences Clone
-// Props Part 1: Understanding the Concept
-
-{/* 
-<html>
-    <head></head>
-    <body>
-        <a href="https://google.com">This is a link</a>
-        <img src="" />
-        <input placeholder="First Name" type="submit"/>
-    </body>
-</html>
-
-function addTwoNumbersTogether(a,b) {
-    return a + b
-}
-
-// 제한적인 기능
-// 매개변수를 넣어서 동적으로 변하게 한다 
-*/}
-
-// 2. Build an AirBnb Experiences Clone
-// Props Part 2: Reusable Components
-
-// 2. Build an AirBnb Experiences Clone
-// Aside: JS Inside JSX
