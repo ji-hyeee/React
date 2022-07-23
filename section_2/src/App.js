82e4d43a2d2e2a9bbea0b167b95d78ee0ab56e23
sulmeee <poorooria@naver.com>
2022-07-23 16:58:18 +0900

    learn props

--- section_2/src/App.js
+++ section_2/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./compoenets/NavBar"
 import Hero from "./compoenets/Hero"
 import Card from "./compoenets/Card"
+import Contact from "./compoenets/Contact"
 
 export default function App() {
   return (
@@ -9,6 +10,34 @@ export default function App() {
       <Navbar />
       <Hero />
       <Card />
+      <div className="contacts">
+        {/* 2. Build an AirBnb Experiences Clone
+        Props Part 3: Create a contact component */}
+        <Contact
+          img="https://pbs.twimg.com/media/FYVZMQSaMAE4cDO?format=jpg&name=large"
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact
+          img="https://pbs.twimg.com/media/FWE7bHcaQAcSEZF?format=jpg&name=large"
+          name="Fluffykins"
+          phone="(212) 555-2345"
+          email="fluff@me.com"
+        />
+        <Contact
+          img="https://pbs.twimg.com/media/FYQwLBxaMAA2bGU?format=jpg&name=large"
+          name="Felix"
+          phone="(212) 555-4567"
+          email="thecat@hotmail.com"
+        />
+        <Contact
+          img="https://pbs.twimg.com/media/FXS8wjmakAAtJ54?format=jpg&name=large"
+          name="Pumpkin"
+          phone="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+        />
+      </div>
     </div>
   )
 }
---
+++ section_2/src/compoenets/Contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Contact() {
+    return (
+        <div className="contact-card">
+            <img src="https://pbs.twimg.com/media/FYVZMQSaMAE4cDO?format=jpg&name=large" alt="img" />
+            <h3>Mr. Whiskerson</h3>
+            <div className="info-group">
+                <p>(212) 555-1234</p>
+            </div>
+            <div className="info-group">
+                <p>mr.whiskaz@catnap.meow</p>
+            </div>
+        </div>
+    )
+}
