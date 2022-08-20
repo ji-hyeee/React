4ea731d7c3a401b001c2b936456d1224b1977066
sulmeee <poorooria@naver.com>
2022-08-20 18:09:52 +0900

    section 3 intro

---
+++ section_3/section_3/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
+
+// 3. Build a Meme Generator
+// Section intro and Figma File
+
+// Interactive web apps in React
+
+// So far...
+// Static websites
+// Reusable componenets
+
+// Static Web Pages
+// Read-onplay, no change to data
+// ex) blogs, news sites, recipies, etc
+
+// Dynamic Web Apps
+// Read-write: ability to change data
+// Highly icteractive
+// Displays your data
+// ex) bank website, airbnb, etc
