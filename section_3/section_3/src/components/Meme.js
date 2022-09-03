5418e8fe2a1db8a39217740e9d618ac964588594
sulmeee <poorooria@naver.com>
2022-09-03 19:10:41 +0900

    Challenge: flipping State back and forth

--- section_3/section_3/src/components/Meme.js
+++ section_3/section_3/src/components/Meme.js
@@ -40,37 +40,69 @@ export default function Meme() {
 }
 
 // 3. Build a Meme Generator
-// Challenge: Ternary Practice
-// 응용: 삼항 연습
+// Challenge: flipping State back and forth
+// 과제: 상태를 앞뒤로 뒤집기
 
-/**
- * Challenge: Replace the if/else below with a ternary
- * to determine the text that should display on the page
- * 과제: 아래의 if/else를 삼항으로 대체하여 페이지에 표시할 텍스트를 결정합니다.
- */
+// export default function App() {
+    /**
+     * Challenge: 
+     * - Initialize state for `isGoingOut` as a boolean
+     * - Make it so clicking the div.state--value flips that
+     *   boolean value (true -> false, false -> true)
+     * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+     * 
+     * - 'isGoingOut'의 상태를 부울로 초기화
+     * - div.state--value를 클릭하면 해당 부울 값이 뒤집힙니다(true -> false, false -> true).
+     * - `isGoingOut`이 `true`이면 "Yes"를 표시하고, 그렇지 않으면 "No"를 표시합니다.
+     */
+    
+// ref [use callback] - cuz we use previous state
+//     const [isGoingOut, setIsGoingOut] = React.useState(true);
+    
+//     const click = ()=>{
+//         setIsGoingOut(e=>!e)
+//     }
+    
+//     return (
+//         <div className="state">
+//             <h1 className="state--title">Do I feel like going out tonight?</h1>
+//             <div className="state--value" onClick={click}>
+//                 <h1>{isGoingOut?"Yes":"No"}</h1>
+//             </div>
+//         </div>
+//     )
+// }
 
-// const TorF = false;
+// 처음 푼 코드
+//     const [isGoingOut, setIsGoingOut] = React.useState(true);
+//     const [yes, setYes] = React.useState('Yes');
+    
+//     const click = ()=>{
+//         isGoingOut ? setIsGoingOut(false) : setIsGoingOut(true)
+//         isGoingOut ? setYes('Yes') : setYes('No')
+//     }
+    
+//     return (
+//         <div className="state">
+//             <h1 className="state--title">Do I feel like going out tonight?</h1>
+//             <div className="state--value" onClick={click}>
+//                 <h1>{yes}</h1>
+//             </div>
+//         </div>
+//     )
 
-// let answer = (TorF === true) ? "Yes" : "No"
+// 번뜩 떠오른 코드
+// const [isGoingOut, setIsGoingOut] = React.useState(true);
 
-// return(
-//     <div>{answer}</div>
-// )
+// const click = ()=>{
+//     isGoingOut ? setIsGoingOut(false) : setIsGoingOut(true)
+// }
 
-/**
-* Challenge: move our ternary directly inside of the JSX
-* so the "Yes" and "No" are determined inside the <h1>
-*
-* Hint: you will no longer need the `answer` variable
-*
-* 과제: 삼항을 JSX 내부로 직접 이동
-* 그래서 "Yes"와 "No"는 <h1> 내부에서 결정됩니다.
-*
-* 힌트: 더 이상 `answer` 변수가 필요하지 않습니다.
-*/
-
-// const TorF = true
-
-// return(
-//     <h1>{TorF ? "Yes" : "No"}</h1>
+// return (
+//     <div className="state">
+//         <h1 className="state--title">Do I feel like going out tonight?</h1>
+//         <div className="state--value" onClick={click}>
+//             <h1>{isGoingOut?"Yes":"No"}</h1>
+//         </div>
+//     </div>
 // )
