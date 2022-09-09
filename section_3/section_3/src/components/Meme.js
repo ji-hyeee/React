3ca416dbd6ecb6ec19a54257a1408fc4574b5cca
sulmeee <poorooria@naver.com>
2022-09-09 17:04:52 +0900

    Complex State Objects

--- section_3/section_3/src/components/Meme.js
+++ section_3/section_3/src/components/Meme.js
@@ -40,39 +40,47 @@ export default function Meme() {
 }
 
 // 3. Build a Meme Generator
-// Complex State Array
-// 복잡한 상태 배열
+// Complex State: Objects
 
-// function App() {
-//     /**
-//      * Challenge: Convert the code below to use an array
-//      * held in state instead of a local variable. Initialize 
-//      * the state array with the same 2 items below
-//      * 
-//      * Don't worry about fixing `addItem` quite yet.
-//      * 
-//      * * 과제: 지역 변수 대신 상태에 보관된 배열을 사용하도록 아래 코드를 변환하십시오. 아래의 동일한 2개 항목으로 상태 배열을 초기화합니다.
-//       *
-//       * 아직 `addItem`을 수정하는 것에 대해 걱정하지 마십시오.
-//      */
-//     // const thingsArray = ["Thing 1", "Thing 2"]
-//     const [state, setState] = React.useState(["Thing 1", "Thing 2"])
+// import React from "react"
 
-//     function addItem() {
-//         // We'll work on this next
-//         // const newThingText = `Thing ${thingsArray.length + 1}`
-//         // thingsArray.push(newThingText)
-//         // document.getElementById()
-//         // console.log(thingsArray)
-//         setState(e => [...e, `Thing ${e.length + 1}`])
+// export default function App() {
+//     const [contact, setContact] = React.useState({
+//         firstName: "John",
+//         lastName: "Doe",
+//         phone: "+1 (719) 555-1212",
+//         email: "itsmyrealname@example.com",
+//         isFavorite: false
+//     })
+    
+//     let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+    
+//     function toggleFavorite() {
+//         console.log("Toggle Favorite")
 //     }
-
-//     const thingsElements = state.map(thing => <p key={thing}>{thing}</p>)
-
+    
 //     return (
-//         <div>
-//             <button onClick={addItem}>Add Item</button>
-//             {thingsElements}
-//         </div>
+//         <main>
+//             <article className="card">
+//                 <img src="./images/user.png" className="card--image" />
+//                 <div className="card--info">
+//                     <img 
+//                         src={`../images/${starIcon}`} 
+//                         className="card--favorite"
+//                         onClick={toggleFavorite}
+//                     />
+//                     <h2 className="card--name">
+//                         {contact.firstName} {contact.lastName}
+//                     </h2>
+//                     <p className="card--contact">{contact.phone}</p>
+//                     <p className="card--contact">{contact.email}</p>
+//                 </div>
+                
+//             </article>
+//         </main>
 //     )
 // }
+
+// useState 객체 사용하기
+// 상태.키 로 값을 불러 옵니다
+// 삼항연산자를 사용해서 별 아이콘의 색상을 바꿀 수 있습니다
