f0e33e737422ac842ac0c26d6da44ad8f5f0d1a2
sulmeee <poorooria@naver.com>
2022-09-21 10:43:50 +0900

    Refactor State

--- section_3/section_3/src/components/Meme.js
+++ section_3/section_3/src/components/Meme.js
@@ -3,13 +3,23 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    // const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
-
+        const url = memesArray[randomNumber].url
+        setMeme(preMeme => ({
+            ...preMeme,
+            randomImage: url
+        }))
     }
 
     return (
@@ -33,54 +43,8 @@ export default function Meme() {
                 </button>
             </form>
             <div className="meme_image" >
-                <img src={memeImage} alt='meme_image' />
+                <img src={meme.randomImage} alt='meme_image' />
             </div>
         </main>
     )
 }
-
-// 3. Build a Meme Generator
-// Complex State: Objects
-
-// import React from "react"
-
-// export default function App() {
-//     const [contact, setContact] = React.useState({
-//         firstName: "John",
-//         lastName: "Doe",
-//         phone: "+1 (719) 555-1212",
-//         email: "itsmyrealname@example.com",
-//         isFavorite: false
-//     })
-    
-//     let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
-    
-//     function toggleFavorite() {
-//         console.log("Toggle Favorite")
-//     }
-    
-//     return (
-//         <main>
-//             <article className="card">
-//                 <img src="./images/user.png" className="card--image" />
-//                 <div className="card--info">
-//                     <img 
-//                         src={`../images/${starIcon}`} 
-//                         className="card--favorite"
-//                         onClick={toggleFavorite}
-//                     />
-//                     <h2 className="card--name">
-//                         {contact.firstName} {contact.lastName}
-//                     </h2>
-//                     <p className="card--contact">{contact.phone}</p>
-//                     <p className="card--contact">{contact.email}</p>
-//                 </div>
-                
-//             </article>
-//         </main>
-//     )
-// }
-
-// useState 객체 사용하기
-// 상태.키 로 값을 불러 옵니다
-// 삼항연산자를 사용해서 별 아이콘의 색상을 바꿀 수 있습니다
